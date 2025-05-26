2bb3dedab4f91b549598e03186d3ed82b68c40d3
muc\QXZ2BZO <florian.sc.schmidt@partner.bmw.de>
2025-05-26 16:43:59 +0200

    [SMA-267] update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,47 @@ plugins{
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.kover) apply false
 }
+
+tasks.register("detektAll") {
+    group = "verification"
+    description = "Runs all detekt tasks in all projects"
+
+    val detektTasks = listOf("detekt", "detektMain", "detektTest",
+        "detektJs" , "detektJsMain", "detektJsTest",
+        "detektJvm" , "detektJvmMain", "detektJvmTest",
+    )
+
+    dependsOn(
+        rootProject.allprojects.flatMap { project ->
+            project.tasks.matching {
+                if(it.name in detektTasks){ println("${project.name}:${it.name}")}
+                it.name in detektTasks
+            }
+        }
+    )
+}
+
+tasks.register("detektBaselineAll") {
+    group = "verification"
+    description = "Runs all detekt tasks in all projects"
+
+    val detektTasks = listOf("detektBaseline", "detektBaselineMain", "detektBaselineTest",
+        "detektBaselineJs" , "detektBaselineJsMain", "detektBaselineJsTest",
+        "detektBaselineJvm" , "detektBaselineJvmMain", "detektJvmTest",
+    )
+/*
+    detektTasks.forEach { taskName ->
+        tasks.named<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>(taskName).configure {
+            baseline.set(file("config/detekt/$taskName.xml"))
+        }
+    }
+*/
+    dependsOn(
+        rootProject.allprojects.flatMap { project ->
+            project.tasks.matching {
+                if(it.name in detektTasks){ println("${project.name}:${it.name}")}
+                it.name in detektTasks
+            }
+        }
+    )
+}
