309d93a5518fe03ccecda43a5bac91d8bed5fc1c
drx <schmidt@alpha-structure.com>
2025-08-03 15:16:53 +0200

    [SMA-000] delete obsolete scripts

--- script/rebuild-backend.sh
+++
@@ -1,6 +0,0 @@
-cd ..
-./gradlew solawi-bid-backend:clean && \
-./gradlew solawi-bid-backend:build && \
-./gradlew solawi-bid-backend:buildFatJar && \
-docker compose -p solawi-bid down --remove-orphans && \
-docker compose -p solawi-bid up -d --build solawi-bid_backend
