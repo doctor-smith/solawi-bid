4cfb94e59a363fcf3ed794cca259b9363378d8b7
drx <schmidt@alpha-structure.com>
2025-08-03 15:16:53 +0200

    [SMA-000] delete obsolete scripts

--- script/rebuild-be-and-fe.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-./gradlew solawi-bid-backend:clean && \
-./gradlew solawi-bid-backend:build && \
-./gradlew solawi-bid-backend:buildFatJar && \
-./gradlew solawi-bid-frontend:clean && \
-./gradlew solawi-bid-frontend:build && \
-# -x test -x jsBrowserTest -x jsTest&& \
-docker compose -p solawi-bid down --remove-orphans && \
-docker compose -p solawi-bid up -d --build solawi-bid_backend --build solawi-bid_frontend
