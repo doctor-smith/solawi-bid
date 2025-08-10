e979fe9e90666fa4223d9b4cdf316acce8ae8924
drx <schmidt@alpha-structure.com>
2025-08-10 11:53:04 +0200

    [SMA-318] Make User auditable

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/user/schema/user-management.user.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/user/schema/user-management.user.kt
@@ -3,23 +3,30 @@ package org.solyton.solawi.bid.module.user.schema
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.UUIDTable
+import org.joda.time.DateTime
+import org.solyton.solawi.bid.module.auditable.AuditableEntity
+import org.solyton.solawi.bid.module.auditable.AuditableUUIDTable
 import java.util.*
 
 typealias UserEntity = User
 typealias UsersTable = Users
 
-object Users : UUIDTable("users") {
+object Users : AuditableUUIDTable("users")  {
     val username = varchar("username", 50).uniqueIndex()
     val password = varchar("varchar", 500)
 }
 
 
-class User(id: EntityID<UUID>) : UUIDEntity(id) {
+class User(id: EntityID<UUID>) : UUIDEntity(id), AuditableEntity<UUID> {
     companion object : UUIDEntityClass<User>(Users)
 
     var username by Users.username
     var password by Users.password
+
+    override val createdAt: DateTime by Users.createdAt
+    override val createdBy: UUID by Users.createdBy
+    override val modifiedAt: DateTime? by Users.modifiedAt
+    override val modifiedBy: UUID? by Users.modifiedBy
 
     var organizations by Organization via UserOrganization
 }
