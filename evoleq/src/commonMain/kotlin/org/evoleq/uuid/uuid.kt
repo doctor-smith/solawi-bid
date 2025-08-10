05ec20c4e9bcafdff09da8e370303aef614ea2ed
drx <schmidt@alpha-structure.com>
2025-08-10 11:49:24 +0200

    [SMA-318] Introduce nil uuid

--- evoleq/src/commonMain/kotlin/org/evoleq/uuid/uuid.kt
+++ evoleq/src/commonMain/kotlin/org/evoleq/uuid/uuid.kt
@@ -2,6 +2,7 @@ package org.evoleq.uuid
 
 import com.benasher44.uuid.uuidFrom
 
+const val NIL_UUID = "00000000-0000-0000-0000-000000000000"
 
 fun String.isUuid(): Boolean = try {
     uuidFrom(this)
