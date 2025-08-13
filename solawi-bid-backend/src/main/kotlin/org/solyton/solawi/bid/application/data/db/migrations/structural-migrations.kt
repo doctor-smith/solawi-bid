4415145a515c154622a45007b6a316ad463fdf8d
drx <schmidt@alpha-structure.com>
2025-08-13 23:36:20 +0200

    [SMA-318, SMA-324] Adapt auditable interface

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/application/data/db/migrations/structural-migrations.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/application/data/db/migrations/structural-migrations.kt
@@ -3,14 +3,26 @@ package org.solyton.solawi.bid.application.data.db.migrations
 import org.evoleq.exposedx.migrations.AddMissingColumns
 import org.evoleq.exposedx.migrations.ColumnDef
 import org.evoleq.exposedx.migrations.StructuralMigrations
+import org.solyton.solawi.bid.module.bid.schema.AuctionsTable
 import org.solyton.solawi.bid.module.user.schema.UsersTable
 import java.util.UUID
 
 
 val structuralMigrations by lazy {
     StructuralMigrations(
-        addMissingColumns = listOf(AddMissingColumns(
-            UsersTable, listOf(ColumnDef<UUID>("CREATED_BY", UUID(0L, 0L)))
-        ))
+        addMissingColumns = listOf(
+            AddMissingColumns(
+                UsersTable,
+                listOf(
+                    ColumnDef<UUID>("CREATED_BY", UUID(0L, 0L))
+                )
+            ),
+            AddMissingColumns(
+                AuctionsTable,
+                listOf(
+                    ColumnDef<UUID>("CREATED_BY", UUID(0L, 0L))
+                )
+            )
+        )
     )
 }
