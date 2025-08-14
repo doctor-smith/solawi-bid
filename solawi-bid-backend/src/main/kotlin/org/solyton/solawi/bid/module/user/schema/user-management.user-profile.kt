7719b5252011b1bc4a2c1f1e3c0f107b2ced332b
drx <schmidt@alpha-structure.com>
2025-08-15 00:07:13 +0200

    [SMA-318, SMA-334] Make UserProfiles auditable and add structural migrations

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/user/schema/user-management.user-profile.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/user/schema/user-management.user-profile.kt
@@ -3,14 +3,16 @@ package org.solyton.solawi.bid.module.user.schema
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.UUIDTable
+import org.joda.time.DateTime
+import org.solyton.solawi.bid.module.auditable.AuditableEntity
+import org.solyton.solawi.bid.module.auditable.AuditableUUIDTable
 import java.util.*
 
 typealias UserProfilesTable = UserProfiles
 typealias UserProfileEntity = UserProfile
 
 
-object UserProfiles : UUIDTable("user_profiles") {
+object UserProfiles : AuditableUUIDTable("user_profiles") {
     val userId = reference("user_id", Users)
 
     // val addressId = reference("address_id", Addresses).nullable()
@@ -20,7 +22,7 @@ object UserProfiles : UUIDTable("user_profiles") {
     // val bankAccountId = reference("bank_account_id", BankAccounts).nullable()// nullable default
 }
 
-class UserProfile(id: EntityID<UUID>) : UUIDEntity(id) {
+class UserProfile(id: EntityID<UUID>) : UUIDEntity(id), AuditableEntity<UUID> {
     companion object : UUIDEntityClass<UserProfile>(UserProfiles)
 
     var user by User referencedOn UserProfiles.userId
@@ -32,4 +34,10 @@ class UserProfile(id: EntityID<UUID>) : UUIDEntity(id) {
     // var bankAccount by BankAccount optionalReferencedOn UserProfiles.bankAccountId
 
     // val shares: SizedIterable<Share> by Share.Companion referrersOn Shares.userProfileId
+
+
+    override var createdAt: DateTime by UserProfiles.createdAt
+    override var createdBy: UUID by UserProfiles.createdBy
+    override var modifiedAt: DateTime? by UserProfiles.modifiedAt
+    override var modifiedBy: UUID? by UserProfiles.modifiedBy
 }
