a1161b24e4b07d349a4d77f1229b48fec25fe982
drx <schmidt@alpha-structure.com>
2025-08-14 07:43:06 +0200

    [SMA-318, SMA-326] Make round auditable

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/bid/schema/bid.round.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/bid/schema/bid.round.kt
@@ -3,27 +3,34 @@ package org.solyton.solawi.bid.module.bid.schema
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.UUIDTable
+import org.joda.time.DateTime
+import org.solyton.solawi.bid.module.auditable.AuditableEntity
+import org.solyton.solawi.bid.module.auditable.AuditableUUIDTable
 import org.solyton.solawi.bid.module.bid.data.api.RoundState
 import java.util.*
 
 typealias RoundEntity = Round
 typealias RoundsTable = Rounds
 
-object Rounds: UUIDTable("rounds") {
+object Rounds: AuditableUUIDTable("rounds") {
     val link = varchar("link", 500).default("not-set")
     val state = varchar("state", 100).default("${RoundState.Opened}")
     val auction = reference("auction_id", Auctions)
 }
 
 
-class Round(id: EntityID<UUID>) : UUIDEntity(id) {
+class Round(id: EntityID<UUID>) : UUIDEntity(id), AuditableEntity<UUID> {
     companion object : UUIDEntityClass<Round>(Rounds)
 
     var link by Rounds.link
     var state by Rounds.state
     var auction by Auction referencedOn Rounds.auction
     val bidRounds by BidRound referrersOn BidRounds.auction
+
+    override var createdAt: DateTime by Rounds.createdAt
+    override var createdBy: UUID by Rounds.createdBy
+    override var modifiedAt: DateTime? by Rounds.modifiedAt
+    override var modifiedBy: UUID? by Rounds.modifiedBy
 }
 
 
