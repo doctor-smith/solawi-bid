3267fffa97e11c614da7614504594de1761e71be
drx <schmidt@alpha-structure.com>
2025-08-14 23:02:19 +0200

    [SMA-318, SMA-330] Make Shares and ShareTypes auditable

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/bid/schema/bid.share-type.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/bid/schema/bid.share-type.kt
@@ -3,13 +3,15 @@ package org.solyton.solawi.bid.module.bid.schema
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.UUIDTable
+import org.joda.time.DateTime
+import org.solyton.solawi.bid.module.auditable.AuditableEntity
+import org.solyton.solawi.bid.module.auditable.AuditableUUIDTable
 import java.util.*
 
 typealias ShareTypesTable = ShareTypes
 typealias ShareTypeEntity = Share
 
-object ShareTypes : UUIDTable("share_types") {
+object ShareTypes : AuditableUUIDTable("share_types") {
     val name = varchar("name", 250)
     val description = varchar("description", 5000).default("")
     val fixedPrize = double("fixed_price").nullable()
@@ -17,12 +19,17 @@ object ShareTypes : UUIDTable("share_types") {
     // val currency = varchar("currency", 10).default("Euro") <- import exposed package
 }
 
-class ShareType(id : EntityID<UUID>) : UUIDEntity(id) {
+class ShareType(id : EntityID<UUID>) : UUIDEntity(id), AuditableEntity<UUID> {
     companion object : UUIDEntityClass<ShareType>(ShareTypes)
 
     var name by ShareTypes.name
     var description by ShareTypes.description
     var fixedPrize by ShareTypes.fixedPrize
     var ahcAuthorizationRequired by ShareTypes.ahcAuthorizationRequired
+
+    override var createdAt: DateTime by ShareTypes.createdAt
+    override var createdBy: UUID by ShareTypes.createdBy
+    override var modifiedAt: DateTime? by ShareTypes.modifiedAt
+    override var modifiedBy: UUID? by ShareTypes.modifiedBy
 }
 
