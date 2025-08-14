8ca83025dee92a5e177de94b4d4869e93c10cd43
drx <schmidt@alpha-structure.com>
2025-08-14 21:23:15 +0200

    [SMA-318, SMA-328] Make AuctionDetailsSolawiTuebingen auditable - add structural migration

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/application/data/db/migrations/structural-migrations.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/application/data/db/migrations/structural-migrations.kt
@@ -4,6 +4,7 @@ import org.evoleq.exposedx.migrations.AddMissingColumns
 import org.evoleq.exposedx.migrations.ColumnDef
 import org.evoleq.exposedx.migrations.StructuralMigrations
 import org.evoleq.uuid.UUID_ZERO
+import org.solyton.solawi.bid.module.bid.schema.AuctionDetailsSolawiTuebingenTable
 import org.solyton.solawi.bid.module.bid.schema.AuctionsTable
 import org.solyton.solawi.bid.module.bid.schema.RoundsTable
 import org.solyton.solawi.bid.module.user.schema.OrganizationsTable
@@ -22,6 +23,12 @@ val structuralMigrations by lazy {
             ),
             AddMissingColumns(
                 AuctionsTable,
+                listOf(
+                    ColumnDef<UUID>("CREATED_BY", UUID_ZERO)
+                )
+            ),
+            AddMissingColumns(
+                AuctionDetailsSolawiTuebingenTable,
                 listOf(
                     ColumnDef<UUID>("CREATED_BY", UUID_ZERO)
                 )
