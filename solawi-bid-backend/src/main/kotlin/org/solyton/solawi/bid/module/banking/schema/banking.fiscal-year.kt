3577ec3ea4b93a5b6ca874559995d8a59924d28f
drx <schmidt@alpha-structure.com>
2025-08-14 23:20:53 +0200

    [SMA-318, SMA-332] Make FiscalYears auditable and add structural migrations

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/banking/schema/banking.fiscal-year.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/banking/schema/banking.fiscal-year.kt
@@ -3,23 +3,30 @@ package org.solyton.solawi.bid.module.banking.schema
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.jodatime.date
+import org.joda.time.DateTime
+import org.solyton.solawi.bid.module.auditable.AuditableEntity
+import org.solyton.solawi.bid.module.auditable.AuditableUUIDTable
 import java.util.*
 
 typealias FiscalYearsTable = FiscalYears
 typealias FiscalYearEntity = FiscalYear
 
-object FiscalYears : UUIDTable("fiscal_years") {
+object FiscalYears : AuditableUUIDTable("fiscal_years") {
 
     val start = date("start")
     val end = date("end")
 
 }
 
-class FiscalYear(id: EntityID<UUID>) : UUIDEntity(id) {
+class FiscalYear(id: EntityID<UUID>) : UUIDEntity(id), AuditableEntity<UUID> {
     companion object : UUIDEntityClass<FiscalYear>(FiscalYears)
 
     var start by FiscalYears.start
     var end by FiscalYears.end
+
+    override var createdAt: DateTime by FiscalYears.createdAt
+    override var createdBy: UUID by FiscalYears.createdBy
+    override var modifiedAt: DateTime? by FiscalYears.modifiedAt
+    override var modifiedBy: UUID? by FiscalYears.modifiedBy
 }
