4efdb4187e0f77f153a095c39522fa6ad51f0272
drx <schmidt@alpha-structure.com>
2025-08-16 11:10:28 +0200

    [SMA-341] Refactor ColumnDef and introduce ColumnDef.Missing, ColumnDef.ModifyName

--- solawi-bid-backend/src/main/kotlin/org/evoleq/exposedx/migrations/structural/dsl-structural-migrations.kt
+++ solawi-bid-backend/src/main/kotlin/org/evoleq/exposedx/migrations/structural/dsl-structural-migrations.kt
@@ -2,7 +2,7 @@ package org.evoleq.exposedx.migrations.structural
 
 import org.jetbrains.exposed.sql.Table
 
-fun Table.addColumnsIfMissing(vararg columnDef: ColumnDef<Any?>): AddMissingColumns = AddMissingColumns(
+fun Table.addColumnsIfMissing(vararg columnDef: ColumnDef.Missing<Any?>): AddMissingColumns = AddMissingColumns(
     this,
     listOf(
         *columnDef
