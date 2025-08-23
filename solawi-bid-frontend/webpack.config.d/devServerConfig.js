8c1ce976d63c38da7b1d6238cc356c2eb51baf18
drx <schmidt@alpha-structure.com>
2025-08-23 21:56:32 +0200

    [SMA-169] reload auctions page

--- solawi-bid-frontend/webpack.config.d/devServerConfig.js
+++ solawi-bid-frontend/webpack.config.d/devServerConfig.js
@@ -1,3 +1,5 @@
+const devPath = require("path");
+
 config.devServer = {
   ...config.devServer, // Merge with other devServer settings
 
@@ -5,16 +7,30 @@ config.devServer = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-  //  "X-Content-Type-Options": "nosniff",
-  //  "X-MyHeader": "empty"
   },
-   "historyApiFallback": true
-   /*
-   {
-       rewrites: [
-         { from: "$/hanoi-towers.js", to: 'hanoi-towers.js' },
 
-       ],
-     },
-     */
+   // Serve files like /favicon.ico, /img/*, /css/* directly from public/
+   static: [
+       {
+         directory: devPath.resolve(__dirname, "../solawi-bid-solawi-bid-frontend/kotlin"),
+         publicPath: "/"
+       },
+       {
+         directory: devPath.resolve(__dirname, "../../../../solawi-bid-frontend/build/processedResources/js/main"),
+         publicPath: "/"
+       }
+     ],
+
+   // SPA routing fallback (critical!)
+   historyApiFallback: {
+     rewrites: [
+       {
+         from: /^(?!\/(img|css|assets|i18n|solawi-bid-frontend\.js)).*$/,
+         to: '/index.html'
+       }
+     ],
+     disableDotRule: true  // don't misinterpret UUID/dash routes as files
+   },
+   hot: true,
+   compress: true,
 };
--- solawi-bid-frontend/webpack.config.d/environment.js
+++ solawi-bid-frontend/webpack.config.d/environment.js
@@ -1,5 +1,6 @@
+/*
 var webpack = require("webpack");
-var path = require('path');
+const path = require('path');
 var dotenv = require('dotenv').config({ path: path.resolve(__dirname, '../../../../solawi-bid-frontend/.env') });
 var definePlugin = new webpack.DefinePlugin(
    {
@@ -7,3 +8,27 @@ var definePlugin = new webpack.DefinePlugin(
    }
 );
 config.plugins.push(definePlugin);
+*/
+
+// Imports at the top
+const envPath = require("path");
+const envWebpack = require("webpack");
+const envDotenv = require("dotenv");
+
+module.exports = (config) => {
+  // Load .env file
+  const env = envDotenv.config({
+    path: envPath.resolve(__dirname, "../../../../solawi-bid-frontend/.env")
+  });
+
+  // Add DefinePlugin to inject env variables
+  const definePlugin = new envWebpack.DefinePlugin({
+    PROCESS_ENV: JSON.stringify(env.parsed)
+  });
+
+  // Ensure plugins array exists
+  config.plugins = config.plugins || [];
+  config.plugins.push(definePlugin);
+
+  return config;
+};
