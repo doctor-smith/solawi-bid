3bac1120f31e4545d6511fb4975f6501439498f7
drx <schmidt@alpha-structure.com>
2025-08-28 08:17:54 +0200

    [SMA-346] Add typealiases for tables application_context and module_context

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/application/schema/context_module.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/application/schema/context_module.kt
@@ -3,6 +3,8 @@ package org.solyton.solawi.bid.module.application.schema
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.solyton.solawi.bid.module.permission.schema.ContextsTable
 
+typealias ModuleContextsTable = ModuleContexts
+
 object ModuleContexts : UUIDTable("module_contexts") {
     val moduleId = reference("module_id", ModulesTable)
     val contextId = reference("context_id", ContextsTable)
