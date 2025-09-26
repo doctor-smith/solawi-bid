7cabe2417d6b61bc512003981eadde3f1f2ab53b
drx <schmidt@alpha-structure.com>
2025-09-26 09:49:27 +0200

    [SMA-356] Add actions to frontend + tests

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/application/data/transform.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/application/data/transform.kt
@@ -3,6 +3,7 @@ package org.solyton.solawi.bid.module.application.data
 import org.solyton.solawi.bid.module.application.data.application.Application
 import org.solyton.solawi.bid.module.application.data.module.Module
 import org.solyton.solawi.bid.module.application.data.userapplication.UserApplications
+import org.solyton.solawi.bid.module.permissions.data.relations.ContextRelation
 
 
 fun ApiUserApplications.toDomainType(): List<UserApplications> = map.entries.map {
@@ -31,3 +32,16 @@ fun ApiModule.toDomainType(): Module = Module(
     state = lifecycleStage
 )
 
+fun ApiApplicationContextRelations.toDomainType(): List<ContextRelation> = all.map { it.toDomainType() }
+
+fun ApiApplicationContextRelation.toDomainType(): ContextRelation = ContextRelation(
+    contextId = contextId,
+    relatedId = applicationId
+)
+
+fun ApiModuleContextRelations.toDomainType(): List<ContextRelation> = all.map { it.toDomainType() }
+
+fun ApiModuleContextRelation.toDomainType(): ContextRelation = ContextRelation(
+    contextId = contextId,
+    relatedId = moduleId
+)
