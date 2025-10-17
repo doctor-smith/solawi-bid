0f058e5838abcf0447bd3e66ea9dab1f0c25c657
drx <schmidt@alpha-structure.com>
2025-10-18 00:10:05 +0200

    [SMA-390] Add endpoint to comment on rounds

---
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/bid/action/db/comment-on-round.kt
@@ -0,0 +1,37 @@
+package org.solyton.solawi.bid.module.bid.action.db
+
+import org.evoleq.exposedx.transaction.resultTransaction
+import org.evoleq.ktorx.Contextual
+import org.evoleq.ktorx.DbAction
+import org.evoleq.ktorx.KlAction
+import org.evoleq.ktorx.result.Result
+import org.evoleq.ktorx.result.bindSuspend
+import org.evoleq.math.MathDsl
+import org.evoleq.math.x
+import org.solyton.solawi.bid.module.bid.data.api.ApiRoundComments
+import org.solyton.solawi.bid.module.bid.data.api.CommentOnRound
+import org.solyton.solawi.bid.module.bid.data.api.RoundComments
+import org.solyton.solawi.bid.module.bid.data.toApiType
+import org.solyton.solawi.bid.module.bid.exception.BidRoundException
+import org.solyton.solawi.bid.module.bid.repository.addComment
+import org.solyton.solawi.bid.module.bid.schema.RoundEntity
+import org.solyton.solawi.bid.module.bid.schema.RoundsTable
+import java.util.UUID
+
+@MathDsl
+val CommentOnRound = KlAction<Result< Contextual< CommentOnRound>>, Result<RoundComments>> {
+    result -> DbAction {
+        database -> result bindSuspend  { contextual -> resultTransaction(database){
+            val data = contextual.data
+            val round = RoundEntity.find { RoundsTable.id eq UUID.fromString(data.roundId) }.firstOrNull()
+                ?: throw BidRoundException.NoSuchRound
+
+            round.addComment(
+                data.comment,
+                contextual.userId,
+
+            )
+            ApiRoundComments(round.comments.toList().map { it.toApiType() })
+        } } x database
+    }
+}
