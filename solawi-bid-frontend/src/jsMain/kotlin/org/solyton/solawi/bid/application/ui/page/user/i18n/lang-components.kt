2ec3b6737d01aa62b290efa41ca2952849f09da1
drx <schmidt@alpha-structure.com>
2025-10-30 12:24:08 +0100

    [SMA-402] setup i18n

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/user/i18n/lang-components.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/user/i18n/lang-components.kt
@@ -2,9 +2,18 @@ package org.solyton.solawi.bid.application.ui.page.user.i18n
 
 import org.evoleq.language.LangComponent
 
-private const val basePath = "solyton.user"
+private const val BASE_PATH = "solyton.user"
 
-sealed class UserLangComponent(override val path: String, override val value: String = basePath) : LangComponent {
-    data object UserManagementPage : UserLangComponent("$basePath.managementPage")
-    data object UserPrivatePage : UserLangComponent("$basePath.privatePage")
+sealed class UserLangComponent(override val path: String, override val value: String = BASE_PATH) : LangComponent {
+    data object UserManagementPage : UserLangComponent("$BASE_PATH.managementPage")
+    data object UserPrivatePage : UserLangComponent("$BASE_PATH.privatePage")
+}
+
+private const val ORGANIZATION_BASE_PATH = "$BASE_PATH.organization"
+
+sealed class OrganizationLangComponent(
+    override val path: String,
+    override val value: String = ORGANIZATION_BASE_PATH
+) : LangComponent {
+    data object OrganizationManagementPage : OrganizationLangComponent("$ORGANIZATION_BASE_PATH.managementPage")
 }
