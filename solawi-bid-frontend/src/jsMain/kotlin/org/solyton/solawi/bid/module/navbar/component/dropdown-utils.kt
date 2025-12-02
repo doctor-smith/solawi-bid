61219faadda4ab942a34c0e3af35decf75c44bb9
drx <schmidt@alpha-structure.com>
2025-12-02 06:54:08 +0100

    [SMA-403] Separate dropdown utils

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/navbar/component/dropdown-utils.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/navbar/component/dropdown-utils.kt
@@ -1,8 +1,6 @@
 package org.solyton.solawi.bid.module.navbar.component
 
 import androidx.compose.runtime.Composable
-import io.ktor.client.fetch.AddEventListenerOptions
-import kotlinx.browser.window
 import org.evoleq.compose.Markup
 import org.jetbrains.compose.web.css.DisplayStyle
 import org.jetbrains.compose.web.css.display
@@ -10,8 +8,6 @@ import org.jetbrains.compose.web.css.em
 import org.jetbrains.compose.web.css.width
 import org.jetbrains.compose.web.dom.I
 import org.jetbrains.compose.web.dom.Span
-import org.jetbrains.compose.web.dom.Text
-import org.w3c.dom.events.Event
 
 @Markup
 @Composable
@@ -25,17 +21,3 @@ fun SimpleUpDown(open: Boolean) = Span({
     val icon = if(open) "fa-chevron-left" else "fa-chevron-down"
     I({classes("fa-solid", icon)}){}
 }
-
-@Markup
-@Composable
-fun addDropdownCloseHandler(close: (Event?) -> Unit) {
-    window.addEventListener("click",
-        { event ->
-            close(event)
-        },
-        object : AddEventListenerOptions {
-            override var once: Boolean? = true
-        }
-    )
-}
-
