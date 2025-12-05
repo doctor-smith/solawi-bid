37c52bb883a0386f0f3dbc1f46213d004e5a9ce1
drx <schmidt@alpha-structure.com>
2025-12-05 11:29:10 +0100

    [SMA-403] add helper functions for sequential / pulsing execution of conditional actions

---
+++ solawi-bid-frontend/src/jsTest/kotlin/org/evoleq/compose/guard/sequentially-executed.kt
@@ -0,0 +1,132 @@
+package org.evoleq.compose.guard
+
+import androidx.compose.runtime.Composable
+import org.evoleq.compose.guard.data.ConditionalAction
+import org.evoleq.compose.guard.data.ConditionalActionInput
+import org.evoleq.compose.guard.data.onFulfilled
+import org.evoleq.compose.guard.data.sequentiallyExecuted
+import org.jetbrains.compose.web.testutils.ComposeWebExperimentalTestsApi
+import org.jetbrains.compose.web.testutils.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class SequentialExecutionTest {
+
+    @OptIn(ComposeWebExperimentalTestsApi::class)
+    @Test
+    fun sequentiallyExecutedOneAction() = runTest {
+        val read = { ConditionalActionInput(5,5) }
+        var r1 = 0
+
+        composition {
+            val result = sequentiallyExecuted(
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x < 10 },
+                    action = {a: Int -> r1 = a*a}
+                )
+            )
+            assertTrue { result }
+            assertEquals(25, r1)
+        }
+    }
+
+    @OptIn(ComposeWebExperimentalTestsApi::class)
+    @Test
+    fun sequentiallyExecutedOneAction2() = runTest {
+        val read = { ConditionalActionInput(5,5) }
+        var r1 = 0
+
+        composition {
+            val result = sequentiallyExecuted(
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x > 10 },
+                    action = {a: Int -> r1 = a*a}
+                )
+            )
+            assertFalse { result }
+            assertEquals(0, r1)
+        }
+    }
+
+    @OptIn(ComposeWebExperimentalTestsApi::class)
+    @Test
+    fun sequentiallyExecutedFirstActionOfTwo() = runTest {
+        val read = { ConditionalActionInput(5,5) }
+        var r1 = 0
+        var r2 = 0
+
+        composition {
+            val result = sequentiallyExecuted(
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x < 10 },
+                    action = {a: Int -> r1 = a*a}
+                ),
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x < 10 },
+                    action = {a: Int -> r2 = a*a}
+                )
+            )
+            assertTrue { result }
+            assertEquals(25, r1)
+            assertEquals(0, r2)
+        }
+    }
+
+    @OptIn(ComposeWebExperimentalTestsApi::class)
+    @Test
+    fun sequentiallyExecutedSecondActionOfTwo() = runTest {
+        val read = { ConditionalActionInput(5,5) }
+        var r1 = 0
+        var r2 = 0
+
+        composition {
+            val result = sequentiallyExecuted(
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x > 10 },
+                    action = {a: Int -> r1 = a*a}
+                ),
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x < 10 },
+                    action = {a: Int -> r2 = a*a}
+                )
+            )
+            assertTrue { result }
+            assertEquals(0, r1)
+            assertEquals(25, r2)
+        }
+    }
+
+    @OptIn(ComposeWebExperimentalTestsApi::class)
+    @Test
+    fun sequentiallyExecutedNoActionOfTwo() = runTest {
+        val read = { ConditionalActionInput(5,5) }
+        var r1 = 0
+        var r2 = 0
+
+        composition {
+            val result = sequentiallyExecuted(
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x > 10 },
+                    action = {a: Int -> r1 = a*a}
+                ),
+                onFulfilled(
+                    source = read,
+                    predicate = { x: Int -> x > 10 },
+                    action = {a: Int -> r2 = a*a}
+                )
+            )
+            assertFalse { result }
+            assertEquals(0, r1)
+            assertEquals(0, r2)
+        }
+    }
+}
