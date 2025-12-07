2810af1062768a40fffe1806c94a7cd54b65bf23
drx <schmidt@alpha-structure.com>
2025-12-07 09:27:00 +0100

    [SMA-403, SMA-411] save current context on pagehide

---
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/storage/event/context.kt
@@ -0,0 +1,25 @@
+package org.solyton.solawi.bid.application.storage.event
+
+import kotlinx.browser.window
+import org.evoleq.optics.storage.Storage
+import org.evoleq.optics.transform.times
+import org.solyton.solawi.bid.application.data.Application
+import org.solyton.solawi.bid.application.data.context
+import org.solyton.solawi.bid.module.context.data.current
+import org.solyton.solawi.bid.module.localstorage.api.read
+import org.solyton.solawi.bid.module.localstorage.api.write
+import org.w3c.dom.AddEventListenerOptions
+
+private const val CONTEXT_ID = "context_id"
+
+fun Storage<Application>.checkContext(){
+    val storedContextId: String? = read(CONTEXT_ID)
+    if(storedContextId != null) {
+        (this * context * current).write(storedContextId)
+    }
+    window.addEventListener(
+        type = "pagehide",
+        callback = { write(CONTEXT_ID, (this * context * current).read() )},
+        options =  AddEventListenerOptions(once = true)
+    )
+}
