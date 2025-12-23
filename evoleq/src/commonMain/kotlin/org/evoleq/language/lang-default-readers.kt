a7730c285924d7512913681d3e62e1b9aea8da22
drx <schmidt@alpha-structure.com>
2025-12-23 14:40:49 +0100

    [SMA-418, SMA-419, SMA-420] transfer

--- evoleq/src/commonMain/kotlin/org/evoleq/language/lang-default-readers.kt
+++ evoleq/src/commonMain/kotlin/org/evoleq/language/lang-default-readers.kt
@@ -2,10 +2,14 @@ package org.evoleq.language
 
 import org.evoleq.math.Reader
 
-
+@I18N
 val date: Reader<Lang.Block, String> = Reader { lang -> lang["date"] }
+@I18N
 val description: Reader<Lang.Block, String> = Reader { lang -> lang["description"] }
 val subTitle: Reader<Lang.Block, String> = Reader { lang -> lang["subTitle"] }
+@I18N
 val title: Reader<Lang.Block, String> = Reader { lang -> lang["title"] }
+@I18N
 val text: Reader<Lang.Block, String> = Reader { lang -> lang["text"] }
+@I18N
 val tooltip: Reader<Lang.Block, String> = Reader{ lang -> lang["tooltip"]}
