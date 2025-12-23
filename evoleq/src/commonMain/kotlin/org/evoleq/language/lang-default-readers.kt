e341caa5caf223813ce65dbb25dc28fa63e53ff6
drx <schmidt@alpha-structure.com>
2025-12-23 09:22:02 +0100

    [SMA-418, SMA-419, SMA-420] application-page withLoading and i18n

--- evoleq/src/commonMain/kotlin/org/evoleq/language/lang-default-readers.kt
+++ evoleq/src/commonMain/kotlin/org/evoleq/language/lang-default-readers.kt
@@ -2,8 +2,10 @@ package org.evoleq.language
 
 import org.evoleq.math.Reader
 
-val title: Reader<Lang.Block, String> = Reader { lang -> lang["title"] }
-val subTitle: Reader<Lang.Block, String> = Reader { lang -> lang["subTitle"] }
+
 val date: Reader<Lang.Block, String> = Reader { lang -> lang["date"] }
+val description: Reader<Lang.Block, String> = Reader { lang -> lang["description"] }
+val subTitle: Reader<Lang.Block, String> = Reader { lang -> lang["subTitle"] }
+val title: Reader<Lang.Block, String> = Reader { lang -> lang["title"] }
 val text: Reader<Lang.Block, String> = Reader { lang -> lang["text"] }
 val tooltip: Reader<Lang.Block, String> = Reader{ lang -> lang["tooltip"]}
--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/application/management/application-page.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/ui/page/application/management/application-page.kt
@@ -10,10 +10,12 @@ import org.evoleq.compose.Markup
 import org.evoleq.compose.guard.data.isLoading
 import org.evoleq.compose.guard.data.onEmpty
 import org.evoleq.compose.guard.data.onNullLaunch
+import org.evoleq.compose.guard.data.withLoading
 import org.evoleq.compose.layout.Horizontal
 import org.evoleq.compose.routing.navigate
 import org.evoleq.device.data.mediaType
 import org.evoleq.language.component
+import org.evoleq.language.description
 import org.evoleq.language.subComp
 import org.evoleq.language.title
 import org.evoleq.language.tooltip
@@ -44,6 +46,8 @@ import org.solyton.solawi.bid.module.application.data.management.availableApplic
 import org.solyton.solawi.bid.module.application.data.management.personalApplicationContextRelations
 import org.solyton.solawi.bid.module.application.i18n.Component
 import org.solyton.solawi.bid.module.application.i18n.Component.editContext
+import org.solyton.solawi.bid.module.application.i18n.application
+import org.solyton.solawi.bid.module.application.i18n.module
 import org.solyton.solawi.bid.module.control.button.ArrowUpButton
 import org.solyton.solawi.bid.module.control.button.DetailsButton
 import org.solyton.solawi.bid.module.control.button.EditButton
@@ -68,66 +72,78 @@ import org.solyton.solawi.bid.module.user.data.actions as userActions
 @Markup
 @Composable
 @Suppress("FunctionName")
-fun ApplicationPage(storage: Storage<Application>, applicationId: String) = Div {
-
-    if (isLoading(
-            onEmpty(storage * applicationManagementModule * availableApplications.get) {
-                CoroutineScope(Job()).launch {
-                    (storage * applicationManagementModule * applicationManagementActions).dispatch(
-                        readApplications
-                    )
-                }
-            },
-            onEmpty(storage * applicationManagementModule * personalApplicationContextRelations.get) {
-                CoroutineScope(Job()).launch {
-                    (storage * applicationManagementModule * applicationManagementActions).dispatch(
-                        readApplicationContextRelations
-                    )
-                }
-            },
-            onNullLaunch(
-                storage * availablePermissions * contextFromPath("APPLICATION"),
-            ) {
-                CoroutineScope(Job()).launch { (storage * userIso * userActions).dispatch(readUserPermissionsAction()) }
-            },
+fun ApplicationPage(storage: Storage<Application>, applicationId: String) = withLoading(
+    isLoading = isLoading(
+        onEmpty(storage * applicationManagementModule * availableApplications.get) {
+            CoroutineScope(Job()).launch {
+                (storage * applicationManagementModule * applicationManagementActions).dispatch(
+                    readApplications
+                )
+            }
+        },
+        onEmpty(storage * applicationManagementModule * personalApplicationContextRelations.get) {
+            CoroutineScope(Job()).launch {
+                (storage * applicationManagementModule * applicationManagementActions).dispatch(
+                    readApplicationContextRelations
+                )
+            }
+        },
+        onNullLaunch(
+            storage * availablePermissions * contextFromPath("APPLICATION"),
+        ) {
+            CoroutineScope(Job()).launch { (storage * userIso * userActions).dispatch(readUserPermissionsAction()) }
+        },
+        onMissing(
+            ApplicationLangComponent.ApplicationPage,
+            storage * i18N.get
+        ) {
+            LaunchComponentLookup(
+                langComponent = ApplicationLangComponent.ApplicationPage,
+                environment = storage * environment * i18nEnvironment,
+                i18n = (storage * i18N)
+            )
+        },
+        *(storage * applicationManagementModule * availableApplications).read().map {
             onMissing(
-                ApplicationLangComponent.ApplicationPage,
+                ApplicationLangComponent.ApplicationDetails(it.name),
                 storage * i18N.get
-            ) {
+            ){
                 LaunchComponentLookup(
-                    langComponent = ApplicationLangComponent.ApplicationPage,
+                    langComponent = ApplicationLangComponent.ApplicationDetails(it.name),
                     environment = storage * environment * i18nEnvironment,
                     i18n = (storage * i18N)
                 )
             }
-        )
-    ) {Loading()} else {
-
-
+        }.toBooleanArray()
+    ),
+    onLoading = {Loading()}
+){
+    /*
     LaunchedEffect(Unit) {
         launch {
             (storage * userIso * userActions).dispatch(readOrganizations())
         }
     }
+    
+     */
 
+    // Data
     val device = storage * deviceData * mediaType.get
 
-    val texts = storage * i18N * language * component(ApplicationLangComponent.ApplicationPage)
-    val pageTitle = texts * title
-    val allApps = texts * Component.actions * Component.navToAppManPage
-    val listOfModules = texts * Component.listOfModules
-    val defaultContextTexts = texts * Component.defaultContext
-
-/*
-    val organizations = storage * userIso * user * organizations
-    val modals = storage * applicationManagementModule * applicationManagementModals
-*/
     val application = storage * applicationManagementModule * availableApplications * FirstBy { it.id == applicationId }
 
     val contextRelations = (storage * applicationManagementModule * personalApplicationContextRelations).read()
     val defaultContextId = contextRelations.first{ relation -> relation.relatedId == applicationId }.contextId
     val defaultContextPrism = (storage * availablePermissions * contexts).firstByOrNull()
     val defaultContext = defaultContextPrism.match{ it.contextId == defaultContextId }
+
+    // Texts
+    val applicationTexts = storage * i18N * language * subComp(BASE_PATH)
+    val texts = storage * i18N * language * component(ApplicationLangComponent.ApplicationPage)
+    val pageTitle = texts * title
+    val allApps = texts * Component.actions * Component.navToAppManPage
+    val listOfModules = texts * Component.listOfModules
+    val defaultContextTexts = texts * Component.defaultContext
 
     Page({verticalPageStyle() }) {
         Wrap {
@@ -136,7 +152,7 @@ fun ApplicationPage(storage: Storage<Application>, applicationId: String) = Div 
                 alignItems(AlignItems.Center)
                 width(100.percent)
             }) {
-                PageTitle( pageTitle map{ "$it ${application.read().name}"})
+                PageTitle( pageTitle map{ "$it: ${(applicationTexts * application(application.read().name) * title).emit()}"})
                 Horizontal {
                     ArrowUpButton(
                         Color.black,
@@ -149,7 +165,7 @@ fun ApplicationPage(storage: Storage<Application>, applicationId: String) = Div 
                     }
                 }
             }
-            SubTitle("Application Description ...")
+            SubTitle(applicationTexts * application(application.read().name) * description)
         }
 
         ListWrapper{
@@ -164,7 +180,11 @@ fun ApplicationPage(storage: Storage<Application>, applicationId: String) = Div 
             ListItems(application * modules) { module ->
                 ListItemWrapper {
                     DataWrapper {
-                        TextCell(module.name) { width(40.percent) }
+                        TextCell(
+                            applicationTexts *
+                            module(application.read().name, module.name) *
+                            title
+                        ) { width(40.percent) }
 
                     }
                     ActionsWrapper({
@@ -248,4 +268,4 @@ fun ApplicationPage(storage: Storage<Application>, applicationId: String) = Div 
             }
         }
     }
-}}
+}
