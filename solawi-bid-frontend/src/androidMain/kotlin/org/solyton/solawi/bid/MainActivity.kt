8839f42c1a6b2358ab03b8f98bebe5199b3e93e7
drx <schmidt@alpha-structure.com>
2026-01-31 10:18:57 +0100

    [SMA-422,SMA-433,SMA-435,SMA-459] Fix MainActivity imports of material

--- solawi-bid-frontend/src/androidMain/kotlin/org/solyton/solawi/bid/MainActivity.kt
+++ solawi-bid-frontend/src/androidMain/kotlin/org/solyton/solawi/bid/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
