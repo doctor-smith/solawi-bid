515d29d187910000e195d712d61340b30ff53de4
drx <schmidt@alpha-structure.com>
2026-02-01 07:20:20 +0100

    [SMA-422,SMA-433,SMA-435,SMA-459] Adapt import-members-from-csv

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/service/organization/import-members-from-csv.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/service/organization/import-members-from-csv.kt
@@ -3,12 +3,21 @@ package org.solyton.solawi.bid.application.service.organization
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import org.evoleq.csv.parseCsv
+import org.evoleq.csv.parseCsvWithGroupedHeaders
 import org.evoleq.optics.storage.Storage
 import org.evoleq.optics.storage.dispatch
 import org.evoleq.optics.transform.times
 import org.solyton.solawi.bid.application.data.Application
+import org.solyton.solawi.bid.application.data.transform.shares.shareManagementIso
 import org.solyton.solawi.bid.application.data.transform.user.userIso
+import org.solyton.solawi.bid.module.shares.data.api.PricingType
+import org.solyton.solawi.bid.module.shares.data.internal.ShareStatus
+import org.solyton.solawi.bid.module.shared.parser.csv.toColumnType
+import org.solyton.solawi.bid.module.shares.action.importShareSubscriptions
+import org.solyton.solawi.bid.module.shares.data.api.ImportShareSubscription
+import org.solyton.solawi.bid.module.shares.data.mappings.ShareManagementMappings
+import org.solyton.solawi.bid.module.shares.data.shareManagementActions
+import org.solyton.solawi.bid.module.shares.data.toApiType
 import org.solyton.solawi.bid.module.user.action.user.importMembersToOrganization
 import org.solyton.solawi.bid.module.user.action.user.importUserProfiles
 import org.solyton.solawi.bid.module.user.data.api.organization.ImportMembers
@@ -17,41 +26,38 @@ import org.solyton.solawi.bid.module.user.data.api.userprofile.ImportUserProfile
 import org.solyton.solawi.bid.module.user.data.api.userprofile.UserProfileToImport
 import org.solyton.solawi.bid.module.user.data.userActions
 
-fun Storage<Application>.importMembersFromCsv(organizationId: String, csv: String, delimiter: Char = ',') {
-    val memberMaps: List<Map<String, String>> = parseCsv(csv, delimiter.toString())
-    // val columnNames = memberMaps.first().keys
-    val membersToImport = memberMaps.map { it["username"]!! }
+
+fun Storage<Application>.importMembersFromCsv(
+    organizationId: String,
+    csv: String, delimiter: Char = ',',
+    shareManagementMappings: ShareManagementMappings? = null
+) {
+    val typedMemberMaps: List<Map<String, Map<String, String>>> = parseCsvWithGroupedHeaders(csv, delimiter.toString())
+    val membersToImport = typedMemberMaps.map { it["user_profiles"]!!["username"]!! }
     val importMembers = ImportMembers(organizationId, membersToImport)
 
-    val userProfilesToImport = memberMaps.map {
-        entry -> UserProfileToImport(
-            username = entry["username"]!!,
-            firstName = entry["firstname"]!!,
-            lastName = entry["lastname"]!!,
-            title = entry["title"]!!,
-            phoneNumber = entry["phone_number"],
-            address = CreateAddress(
-                recipientName = entry["recipient_name"]!!,
-                organizationName = entry["organization_name"],
-                addressLine1 =  entry["address_line_1"]!!,
-                addressLine2 = entry["address_line_2"]!!,
-                city = entry["city"]!!,
-                stateOrProvince = entry["state_or_province"]!!,
-                postalCode = entry["postal_code"]!!,
-                countryCode = entry["country_code"]!!
+    val userProfilesToImport = typedMemberMaps.map { type ->
+        with(type["user_profiles"]!!) {
+            UserProfileToImport(
+                username = this["username"]!!,
+                firstName = this["firstname"]!!,
+                lastName = this["lastname"]!!,
+                title = this["title"]!!,
+                phoneNumber = this["phone_number"],
+                address = CreateAddress(
+                    recipientName = this["recipient_name"]!!,
+                    organizationName = this["organization_name"],
+                    addressLine1 = this["address_line_1"]!!,
+                    addressLine2 = this["address_line_2"]!!,
+                    city = this["city"]!!,
+                    stateOrProvince = this["state_or_province"]!!,
+                    postalCode = this["postal_code"]!!,
+                    countryCode = this["country_code"]!!
+                )
             )
-        )
+        }
     }
     val importUserProfiles = ImportUserProfiles(userProfilesToImport)
-
-    // val possibleShareTypes =
-        listOf(
-        // (shareType - columns)
-        "vegy" to listOf("number_of_shares", "price_pr_share", "depository", "ahc_authorized", "co_subscribers"),
-        "eggs" to listOf("number_of_shares", "depository", "ahc_authorized", "co_subscribers")
-    )
-
-
 
     val userActionStorage = this * userIso * userActions
     CoroutineScope(Job()).launch {
@@ -60,5 +66,51 @@ fun Storage<Application>.importMembersFromCsv(organizationId: String, csv: Strin
 
     CoroutineScope(Job()).launch {
         userActionStorage.dispatch(importUserProfiles(importUserProfiles))
+    }
+
+    if (shareManagementMappings != null) {
+        val shareSubscriptionsToImport = typedMemberMaps.map {
+            it.filter { (key, _) -> key.startsWith("share_subscriptions") }.map { (key, value) ->
+                // Get column type
+                // share_subscriptions:type.offer
+                val (_, type, keyOfShareType) = key.toColumnType()
+                requireNotNull(type)
+                requireNotNull(keyOfShareType)
+
+                val username: String = it["user_profiles"]!!["username"]!!
+                val shareOfferId: String = shareManagementMappings.shareOffers[keyOfShareType]!!
+                val distributionPoint = shareManagementMappings.distributionPoints[value["distribution_point"]!!]!!
+                val numberOfShares: Int = value["number_of_shares"]!!.toInt()
+                val pricePerShare: Double? = when {
+                    type == PricingType.FLEXIBLE.toString() -> value["price_per_share"]!!.toDouble()
+                    else -> null
+                }
+                val ahcAuthorized: Boolean = value["ahc_authorized"]!!.toBoolean()
+                val status = value["status"]!!
+                val coSubscribers = value["co_subscribers"]?.split(",")?.map { sub -> sub.trim() } ?: emptyList()
+
+                ImportShareSubscription(
+                    shareOfferId = shareOfferId,
+                    username = username,
+                    distributionPointId = distributionPoint,
+                    fiscalYearId = shareManagementMappings.fiscalYearId,
+                    numberOfShares = numberOfShares,
+                    pricePerShare = pricePerShare,
+                    ahcAuthorized = ahcAuthorized,
+                    status = ShareStatus.from(status).toApiType(),
+                    coSubscribers = coSubscribers
+                )
+            }
+        }.flatten()
+
+        val shareManagementActionStorage = this * shareManagementIso * shareManagementActions
+        CoroutineScope(Job()).launch {
+            shareManagementActionStorage.dispatch(importShareSubscriptions(
+                override = shareManagementMappings.override,
+                providerId = organizationId,
+                fiscalYearId = shareManagementMappings.fiscalYearId,
+                shareSubscriptionsToImport = shareSubscriptionsToImport
+            ))
+        }
     }
 }
