44cbb530493dcff3d896d285af5a3dc76474e1b1
drx <schmidt@alpha-structure.com>
2026-02-05 04:32:14 +0100

    [SMA-422,SMA-433,SMA-435,SMA-459] Respect exceptions in transform functions

--- solawi-bid-api-data/src/commonMain/kotlin/org/solyton/solawi/bid/module/user/exception/exception.kt
+++ solawi-bid-api-data/src/commonMain/kotlin/org/solyton/solawi/bid/module/user/exception/exception.kt
@@ -6,6 +6,10 @@ sealed class UserManagementException(override val message: String?) : Exception(
          data class Id(val id: String) : UserDoesNotExist("User with id $id does not exists")
          data class Username(val username: String) : UserDoesNotExist("User with username $username does not exists")
      }
+    sealed class UsersDoNotExist(override val message: String) : UserManagementException(message) {
+        data class Usernames(val usernames: List<String>) : UserDoesNotExist("Users with usernames ${usernames.joinToString(", ") { it }} do not exists")
+        data class Ids(val ids: List<String>) : UsersDoNotExist("Users with ids ${ids.joinToString()} do not exists")
+    }
     data object WrongCredentials : UserManagementException("Wrong credentials")
 
     data class NoSuchUserProfile(val id: String): UserManagementException("No such user profile $id")
