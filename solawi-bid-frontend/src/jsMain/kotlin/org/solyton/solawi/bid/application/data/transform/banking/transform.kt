8dbd5aba9c7fdf91cdbe1d0ef354782a29609695
drx <schmidt@alpha-structure.com>
2026-02-26 05:48:35 +0100

    [SMA-422,SMA-428, SMA-462] Banking

---
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/application/data/transform/banking/transform.kt
@@ -0,0 +1,54 @@
+package org.solyton.solawi.bid.application.data.transform.banking
+
+import org.evoleq.optics.lens.Lens
+import org.evoleq.optics.storage.ActionDispatcher
+import org.evoleq.optics.storage.times
+import org.solyton.solawi.bid.application.data.Application
+import org.solyton.solawi.bid.module.banking.data.application.BankingApplication
+import org.solyton.solawi.bid.module.banking.data.environment.Environment
+import org.solyton.solawi.bid.module.banking.data.user.User
+
+
+val bankingApplicationIso: Lens<Application, BankingApplication> by lazy {
+    Lens<Application, BankingApplication>(
+        get =  {whole -> bankingApplicationPreIso.get(whole).copy(actions = whole.actions * bankingApplicationIso)},
+        set = bankingApplicationPreIso.set
+    )
+}
+
+val bankingApplicationPreIso: Lens<Application, BankingApplication> by lazy {
+    Lens<Application, BankingApplication>(
+        get = { whole ->
+            BankingApplication(
+                environment = with(whole.environment) {
+                    Environment(
+                        type = type,
+                        frontendUrl = frontendUrl,
+                        frontendPort = frontendPort,
+                        backendUrl = backendUrl,
+                        backendPort = backendPort
+                    )
+                },
+                actions = ActionDispatcher {  },
+                modals = whole.modals,
+                deviceData = whole.deviceData,
+                i18N = whole.i18N,
+                user = User(
+                    username = whole.userData.username,
+                    permissions = whole.userData.permissions,
+                    organizations = whole.userData.organizations
+                ),
+                bankAccounts = whole.bankAccounts,
+                fiscalYears = whole.fiscalYears,
+            )
+        },
+        set = { part -> { whole ->
+            whole.copy(
+                modals = part.modals,
+                i18N = part.i18N,
+                bankAccounts = part.bankAccounts,
+                fiscalYears = part.fiscalYears,
+            )
+        } }
+    )
+}
