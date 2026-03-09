e1b2d3754fa0a9fd07fc19cebac495040772f1fc
drx <schmidt@alpha-structure.com>
2026-03-09 19:40:29 +0100

    [SMA-479] remove obsolete println

--- solawi-bid-backend/src/main/kotlin/org/evoleq/exposedx/transaction/ensure-transaction.kt
+++ solawi-bid-backend/src/main/kotlin/org/evoleq/exposedx/transaction/ensure-transaction.kt
@@ -7,10 +7,8 @@ import org.jetbrains.exposed.sql.transactions.transaction
 fun <T> ensureTransaction(statement: Transaction.() -> T): T {
     val tx = TransactionManager.currentOrNull()
     return if (tx != null) {
-        println(">>>>>>>>>>>>>>>>>>>>>>>> Transaction already exists, execute within it")
         tx.statement()
     } else {
-        println(">>>>>>>>>>>>>>>>>>>>>>>> No transaction in context, start a new one")
         // No transaction in context, start a new one
         transaction {
             statement()
