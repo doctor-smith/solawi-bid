96020dc8d37385b18d64a26d0eddd2eb0b57e980
drx <schmidt@alpha-structure.com>
2026-03-21 22:42:30 +0100

    [SMA-490] Add loop control

--- solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/health/routing/routing.kt
+++ solawi-bid-backend/src/main/kotlin/org/solyton/solawi/bid/module/health/routing/routing.kt
@@ -7,7 +7,10 @@ import io.ktor.server.routing.*
 import io.ktor.util.*
 import org.evoleq.exposedx.data.DbEnv
 import org.evoleq.ktorx.data.KTorEnv
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.solyton.solawi.bid.module.health.action.checkApplicationHealth
+import kotlin.coroutines.CoroutineContext
 import kotlin.system.exitProcess
 
 @KtorDsl
@@ -24,11 +27,25 @@ fun <HealthEnv> Routing.health(
                 call.respondText("Application is unhealthy", status = HttpStatusCode.InternalServerError)
             }
         }
-
-        // Endpoint to simulate crash
-        get("/crash") {
-            call.respondText("Application is shutting down...", status = HttpStatusCode.InternalServerError)
-            exitProcess(1) // Simulate a crash by exiting the JVM
+        get("/ready") {
+            try {
+                val database = environment.connectToDatabase()
+                // simple DB-Check
+                newSuspendedTransaction(coroutineContext, database) {
+                    val ready = exec("SELECT 1") { rs ->
+                        rs.next()
+                    }
+                    when(ready) {
+                        true -> call.respondText("Application is ready", status = HttpStatusCode.OK)
+                        false, null -> call.respondText("Application is not ready", status = HttpStatusCode.ServiceUnavailable)
+                    }
+                }
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.ServiceUnavailable)
+            }
+        }
+        get("/live") {
+            call.respondText("Application is live", status = HttpStatusCode.OK)
         }
     }
 }
