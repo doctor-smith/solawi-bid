a6744b3c806288a23053ecd557572369c2541340
drx <schmidt@alpha-structure.com>
2026-07-31 11:17:01 +0200

    [SMA-540] Fix error handling and logic and add tests

---
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/banking/service/service.sepa.payments.next-period.kt
@@ -0,0 +1,34 @@
+package org.solyton.solawi.bid.module.banking.service
+
+import org.solyton.solawi.bid.module.banking.data.sepa.PaymentExecutionStatus
+import org.solyton.solawi.bid.module.banking.data.sepa.SepaSequenceType
+import org.solyton.solawi.bid.module.banking.data.sepa.payment.SepaPayment
+import org.solyton.solawi.bid.module.banking.data.sepa.payment.SepaPaymentHistories
+
+/**
+ * Determines if the current SEPA payment is a candidate for processing in the next payment period.
+ *
+ * A payment is considered a candidate if:
+ * - Its `sequenceType` is not in the forbidden sequence types list.
+ * - There is no successor payment designated for the next period (`nextPeriodSuccessorId` is null).
+ * - If the payment is a retry, its immediate predecessor has not failed (or there is no predecessor).
+ *
+ * @param history A collection of payment history objects that includes predecessors and successors of SEPA payments.
+ * @param forbiddenSeqTypes A list of SEPA sequence types that are not allowed for processing in the next period.
+ * @return `true` if the payment is a candidate for processing in the next period, `false` otherwise.
+ */
+fun SepaPayment.isCandidateForNextPeriodPayment(history:  SepaPaymentHistories, forbiddenSeqTypes: List<SepaSequenceType>): Boolean  =
+    // Payments might be created if
+    // the sequenceType is allowed
+    sequenceType !in forbiddenSeqTypes &&
+    // there is no regular next period payment and
+    nextPeriodSuccessorId == null &&
+    // if the payment is retried, we can create a new one, if it has no failing predecessors
+    when(status){
+        PaymentExecutionStatus.FAILED ->
+            // Check if the payment has a direct failing predecessor
+            history[sepaPaymentId]?.didNotFail()?:false &&
+            // Check if the payment has other failing predecessors
+            history.predecessorOf(sepaPaymentId)?.didNotFail() ?: true
+        else -> true
+    }
