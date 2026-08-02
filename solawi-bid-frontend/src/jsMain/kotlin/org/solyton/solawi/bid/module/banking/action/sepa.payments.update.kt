f973995a27aa3fa7b7e139c72509c140567c248a
drx <schmidt@alpha-structure.com>
2026-08-02 09:27:53 +0200

    [SMA-555] Added action for updating multiple SEPA payments

--- solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/banking/action/sepa.payments.update.kt
+++ solawi-bid-frontend/src/jsMain/kotlin/org/solyton/solawi/bid/module/banking/action/sepa.payments.update.kt
@@ -7,9 +7,12 @@ import org.evoleq.optics.lens.times
 import org.evoleq.optics.storage.Action
 import org.evoleq.optics.storage.suffixed
 import org.evoleq.optics.transform.update
+import org.evoleq.optics.transform.updateAll
 import org.solyton.solawi.bid.module.banking.data.SepaCollectionId
 import org.solyton.solawi.bid.module.banking.data.api.ApiSepaPayment
+import org.solyton.solawi.bid.module.banking.data.api.ApiSepaPayments
 import org.solyton.solawi.bid.module.banking.data.api.UpdateSepaPayment
+import org.solyton.solawi.bid.module.banking.data.api.UpdateSepaPayments
 import org.solyton.solawi.bid.module.banking.data.application.BankingApplication
 import org.solyton.solawi.bid.module.banking.data.application.sepaModule
 import org.solyton.solawi.bid.module.banking.data.sepa.collection.sepaPayments
@@ -17,6 +20,8 @@ import org.solyton.solawi.bid.module.banking.data.sepa.sepaCollections
 import org.solyton.solawi.bid.module.banking.data.toDomainType
 
 const val UPDATE_SEPA_PAYMENT = "UPDATE_SEPA_PAYMENT"
+
+const val UPDATE_SEPA_PAYMENTS = "UPDATE_SEPA_PAYMENTS"
 
 @Markup
 fun updateSepaPayment(
@@ -30,6 +35,22 @@ fun updateSepaPayment(
     writer = (sepaModule * sepaCollections * FirstBy{
         it.sepaCollectionId == targetCollectionId
     } * sepaPayments).update{
-        p,q -> p.sepaPaymentId == q.sepaPaymentId
+            p,q -> p.sepaPaymentId == q.sepaPaymentId
     } contraMap { payment: ApiSepaPayment -> payment.toDomainType()}
+)
+
+@Markup
+fun updateSepaPayments(
+    data: UpdateSepaPayments,
+    targetCollectionId: SepaCollectionId,
+    nameSuffix: String = ""
+) = Action<BankingApplication, UpdateSepaPayments, ApiSepaPayments>(
+    name = UPDATE_SEPA_PAYMENTS.suffixed(nameSuffix),
+    reader = { _ -> data },
+    endPoint = UpdateSepaPayments::class,
+    writer = (sepaModule * sepaCollections * FirstBy{
+        it.sepaCollectionId == targetCollectionId
+    } * sepaPayments).updateAll{
+            p,q -> p.sepaPaymentId == q.sepaPaymentId
+    } contraMap { payments: ApiSepaPayments -> payments.toDomainType()}
 )
