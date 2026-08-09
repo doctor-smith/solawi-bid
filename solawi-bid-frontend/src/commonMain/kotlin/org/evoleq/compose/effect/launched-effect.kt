638b072ddc62f4321760e02134f8d5ff01d2862d
drx <schmidt@alpha-structure.com>
2026-08-09 15:12:54 +0200

    [SMA-566] Extend `LaunchedEffectOnSource` to support multiple keys (up to 3) and refactor related utility functions

--- solawi-bid-frontend/src/commonMain/kotlin/org/evoleq/compose/effect/launched-effect.kt
+++ solawi-bid-frontend/src/commonMain/kotlin/org/evoleq/compose/effect/launched-effect.kt
@@ -8,8 +8,31 @@ import org.evoleq.math.emit
 import org.evoleq.optics.storage.Read
 import org.evoleq.optics.storage.Storage
 
-@Composable fun LaunchedEffectOnSource(key: Source<Any?>, block: suspend CoroutineScope.()->Unit) =
-    LaunchedEffect(key.emit(), block)
+@Composable fun LaunchedEffectOnSource(
+    key: Source<Any?>,
+    block: suspend CoroutineScope.()->Unit
+) = LaunchedEffect(key.emit(), block)
 
-@Composable fun LaunchedEffectOnStorage(key: Storage<Any?>, block: suspend CoroutineScope.()->Unit) =
+@Composable fun LaunchedEffectOnSource(
+    key1: Source<Any?>,
+    key2: Source<Any?>,
+    key3: Source<Any?>,
+    block: suspend CoroutineScope.()->Unit
+) = LaunchedEffect(
+    key1.emit(),
+    key2.emit(),
+    key3.emit(),
+    block
+)
+
+@Composable fun LaunchedEffectOnSource(
+    key1: Source<Any?>,
+    key2: Source<Any?>,
+    block: suspend CoroutineScope.()->Unit
+) = LaunchedEffect(key1.emit(), key2.emit(), block)
+
+@Composable fun LaunchedEffectOnStorage(
+    key: Storage<Any?>,
+    block: suspend CoroutineScope.()->Unit
+) =
     LaunchedEffectOnSource(Read(key), block)
